5c339fd29d8bef458a2887f72d49ba1b281afde8
gdma2026 <gdma2026@163.com>
2025-11-19 18:37:49 +0800

    home

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-498aa77c.js',
+                path_to_root + 'searchindex-459c022e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
